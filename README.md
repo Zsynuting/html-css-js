fd7bfa5e847cd476d6871e0aa1d0b2ad8966fb15
Zsynuting <zsynuting@163.com>
2018-07-08 17:48:38 +0800

    random-string
    Add another implementation of random string, Add test case of performance. Update README

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # html-css-js
 # circle-editor.html
 用 html, javascript, css 实现一个球在圆环轨道内滑动，初始为0，一整圈为100
+
+# random-string.js
+面试题:使用你认为使用性能最高的方式随机生成指定长度的字符串.
+使用了toString(32)方法生成随机字符串,问题是(w/x/y/z)不会出现在随机字符串中
+
+# reduce.js
+面试题:实现一个 Array.prototype.reduce 的 polyfill
--- random-string.js
+++ random-string.js
@@ -20,3 +20,52 @@ module.exports = function (length) {
   }
   return result;
 }
+
+// const getRandomString1 = function (length) {
+//   length = Number(length);
+//   if (isNaN(length)) {
+//     throw new Error("length is not an integer");
+//   } else {
+//     length = Math.floor(length);
+//   }
+//   let result = "";
+//   while (length > 0) {
+//     // pattern is 0.xxxxxxxxxxx, length is 13
+//     // the first character after . is probably 0, so we use the last 10 characters
+//     let pattern = Math.random().toString(32)
+//     result += pattern.substr(length > 10 ? -10 : -length);
+//     length -= 10;
+//   }
+//   return result;
+// }
+
+// const getRandomString2 = function (length) {
+//   length = Number(length);
+//   if (isNaN(length)) {
+//     throw new Error("length is not an integer");
+//   } else {
+//     length = Math.floor(length);
+//   }
+//   let result = "";
+//   while (length-- > 0) {
+//     // pattern is 0.xxxxxxxxxxx, length is 13
+//     // the first character after . is probably 0, so we use the last 10 characters
+//     let index = Math.floor(Math.random() * 36);
+//     result += "0123456789abcdefghijklmnopqrstuvwxyz".indexOf(index);
+//   }
+//   return result;
+// }
+
+// let timer = new Date();
+// for (let i = 0; i < 10000; i++) {
+//   getRandomString1(255);
+// }
+// let end = new Date();
+// console.log(end.getTime() - timer.getTime());
+
+// timer = new Date();
+// for (let i = 0; i < 10000; i++) {
+//   getRandomString2(255);
+// }
+// end = new Date();
+// console.log(end.getTime() - timer.getTime());
